30fd048afb97b024090a48c65aa72944a4d31098
Juan Manuel Gomez <112433289+JuanManuelGomezDev@users.noreply.github.com>
2025-05-08 11:41:16 -0300

    Add files via upload

---
+++ 05 Listas/TP05_Listas.py
@@ -0,0 +1,76 @@
+# EJERCICIO 1
+"""
+multi_de_4 = list(range(4, 101, 4))
+
+
+print(multi_de_4)
+"""
+# EJERCICIO 2
+
+"""
+
+mi_lista = ["Python", "Organizacion empresarial", "Arquitectura de sistemas operativos", "Matematica", "Programacion 1"]
+
+
+print(mi_lista[-2])
+"""
+# EJERCICIO 3
+
+"""
+lista = []
+
+
+lista.append("Buenas")
+lista.append("Tardes")
+lista.append(":D")
+
+
+print(lista)
+"""
+# EJERCICIO 4
+
+"""
+animales = ["perro", "gato", "conejo", "pez"]
+
+
+animales[1] = "loro"
+animales[-1] = "oso"
+
+
+print(animales)
+"""
+# EJERCICIO 5
+
+### EL programa busca el numero mas grande en la lista, lo elimina y luego imprime la lista.
+
+# EJERCICIO 6
+"""
+numeros = list(range(10, 31, 5))  
+print(numeros[:2])  
+"""
+
+# EJERCICIO 7
+"""
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1:3] = ["toyota", "ferrari"]  
+print(autos)
+"""
+# EJERCICIO 8
+"""
+doble = []  
+doble.append(5 * 2)  
+doble.append(10 * 2)  
+doble.append(15 * 2)  
+
+print(doble)  
+"""
+# EJERCICIO 10
+
+
+lista = [15, True, [25.5, 57.9, 30.6], False]
+
+
+print(lista)
+
+
+
